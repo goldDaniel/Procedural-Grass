b70e7664772a0208f120dd760950918012f5e9ac
Daniel Gold <contact@golddaniel.ca>
2020-10-22 01:10:02 -0500

    small scale change

--- GPU Gems 7/Src/Main.cpp
+++ GPU Gems 7/Src/Main.cpp
@@ -54,6 +54,7 @@ struct TerrainSettings
     int chunk_dimensions = 32;
     int max_terrain_height = 512;
     int generation_range = 16.f;
+    int generation_range = 16;
     int scale = 4.f;
 };
 TerrainSettings settings;
